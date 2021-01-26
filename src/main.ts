9752714ae71df5bebc09f5a25df7c7aa0aa4c068
pacokwon <haechank@gmail.com>
2021-01-26 17:14:25 +0900

    update: override bootstrap primary color

--- src/main.ts
+++ src/main.ts
@@ -1,15 +1,19 @@
 import Vue from 'vue';
+import VueApollo from 'vue-apollo';
 import { BootstrapVue, IconsPlugin } from 'bootstrap-vue';
-import 'bootstrap/dist/css/bootstrap.css';
-import 'bootstrap-vue/dist/bootstrap-vue.css';
+import '@/assets/styles.scss';
+
 import App from './App.vue';
 import router from './router';
+import { apolloProvider } from './graphql/apolloProvider';
 
 Vue.config.productionTip = false;
 Vue.use(BootstrapVue);
 Vue.use(IconsPlugin);
+Vue.use(VueApollo);
 
 new Vue({
   router,
+  apolloProvider,
   render: h => h(App)
 }).$mount('#app');
