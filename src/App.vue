5f7e8e3c0db0da105ff2daabb37d2c570725c2a9
pacokwon <haechank@gmail.com>
2021-01-26 18:55:54 +0900

    add: transition classes

--- src/App.vue
+++ src/App.vue
@@ -27,4 +27,30 @@ export default Vue.extend({
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
 }
+
+.fade-enter,
+.fade-leave-to {
+  opacity: 0;
+}
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: all 0.5s ease;
+}
+
+.fade-enter-to,
+.fade-leave {
+  opacity: 1;
+}
+
+.fade-move-enter,
+.fade-move-leave-to {
+  transform: translate(0, 15px);
+  opacity: 0;
+}
+
+.fade-move-enter-active,
+.fade-move-leave-active {
+  transition: all 0.8s ease;
+}
 </style>
