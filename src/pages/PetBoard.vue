a88b8ef0e0c8e7cba16d7e0721ac1bf2cd0d0f16
pacokwon <haechank@gmail.com>
2021-01-28 17:40:44 +0900

    add: infinite scrolling petboard

--- src/pages/PetBoard.vue
+++ src/pages/PetBoard.vue
@@ -1,22 +1,28 @@
 <template>
-  <div
-    class="masonry-container"
-    v-masonry
-    transition-duration="0.4s"
-    gutter="20"
-    item-selector=".masonry-item"
-    column-width="300"
-    fit-width="true"
-  >
-    <pet-card
-      v-masonry-tile
-      v-for="pet in petsList"
-      :key="pet.id"
-      v-bind="pet"
-      class="masonry-item"
-      style="margin-bottom: 20px;"
-    />
-  </div>
+  <section class="petboard-container">
+    <div
+      class="masonry-container"
+      v-masonry
+      transition-duration="0.4s"
+      gutter="20"
+      item-selector=".masonry-item"
+      column-width="300"
+      fit-width="true"
+    >
+      <pet-card
+        v-masonry-tile
+        v-for="pet in petsList"
+        :key="pet.id"
+        v-bind="pet"
+        class="masonry-item"
+        style="margin-bottom: 20px;"
+      />
+    </div>
+    <b-spinner v-if="fetching" variant="primary" type="grow" />
+    <span v-if="!hasNextPage" class="no-more-message">
+      No More Pets!
+    </span>
+  </section>
 </template>
 
 <script lang="ts">
@@ -29,6 +35,29 @@ type PetWithGuardian = Pet & {
   user: User | null;
 };
 
+const petListQuery = gql`
+  query($cursor: String) {
+    pets(after: $cursor) {
+      pageInfo {
+        hasNextPage
+        endCursor
+      }
+      edges {
+        node {
+          id
+          nickname
+          species
+          imageUrl
+          guardian {
+            id
+            nickname
+          }
+        }
+      }
+    }
+  }
+`;
+
 export default Vue.extend({
   name: 'pet-board',
   components: {
@@ -36,40 +65,95 @@ export default Vue.extend({
   },
   data() {
     return {
-      petsList: [] as PetWithGuardian[]
+      petsList: [] as PetWithGuardian[],
+      endCursor: null as null | string,
+      hasNextPage: true,
+      fetching: false
     };
   },
   async mounted() {
+    // send without cursor, to fetch from the start
     const result = await this.$apollo.query({
-      query: gql`
-        query {
-          pets {
-            edges {
-              node {
-                id
-                nickname
-                species
-                imageUrl
-                guardian {
-                  id
-                  nickname
-                }
-              }
-            }
-          }
-        }
-      `
+      query: petListQuery
     });
 
     this.petsList = (
       result?.data?.pets?.edges || []
     ).map(({ node }: Node<PetWithGuardian>) => ({ ...node }));
+
+    const { hasNextPage, endCursor } = result?.data?.pets?.pageInfo || {};
+    this.hasNextPage = hasNextPage;
+    this.endCursor = endCursor;
+
+    window.addEventListener('scroll', this.handleScroll);
+  },
+  destroyed() {
+    window.removeEventListener('scroll', this.handleScroll);
+  },
+  methods: {
+    async handleScroll() {
+      // no need to handle scrolling with no pages left
+      if (!this.hasNextPage || this.fetching) return;
+
+      const FETCH_THRESHOLD = 5;
+
+      // the current vertical amount of scroll
+      const currentHeight = window.scrollY;
+
+      // the entire scrollable height including invisible areas
+      const { scrollHeight } = window.document.body;
+
+      // (effectively) the height of the browser
+      const { clientHeight } = window.document.documentElement;
+
+      // the amount we can scroll through
+      const maxPossibleHeight = scrollHeight - clientHeight;
+
+      /* console.log({ maxPossibleHeight, currentHeight, scrollHeight, clientHeight }) */
+      if (maxPossibleHeight - currentHeight <= FETCH_THRESHOLD) {
+        this.fetching = true;
+        await new Promise(res => setTimeout(res, 1000));
+        await this.fetchMorePets();
+        this.fetching = false;
+      }
+    },
+    async fetchMorePets() {
+      // send without cursor, to fetch from the start
+      const result = await this.$apollo.query({
+        query: petListQuery,
+        variables: {
+          cursor: this.endCursor
+        }
+      });
+
+      const { hasNextPage, endCursor } = result?.data?.pets?.pageInfo || {};
+      this.hasNextPage = hasNextPage;
+      this.endCursor = endCursor;
+
+      const extraPets = (
+        result?.data?.pets?.edges || []
+      ).map(({ node }: Node<PetWithGuardian>) => ({ ...node }));
+
+      this.petsList.push(...extraPets);
+    }
   }
 });
 </script>
 
 <style scoped>
+section.petboard-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 30px;
+}
+
 div.masonry-container {
   margin: 0 auto;
+}
+
+span.no-more-message {
+  font-size: 1.2rem;
+  font-weight: strong;
 }
 </style>
