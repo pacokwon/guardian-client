dd08cd0cda30dccf8d9a5b3ef727972d2bdccded
pacokwon <haechank@gmail.com>
2021-01-28 18:44:16 +0900

    wip: UserCurrentPets component

---
+++ src/components/UserCurrentPets.vue
@@ -0,0 +1,84 @@
+<template>
+  <b-card no-body>
+    <b-card-body>
+      <h4 class="card-title">Current Pets</h4>
+      <div v-if="currentPets.length === 0">
+        This user is currently not registered to any pets.
+      </div>
+      <div v-else>
+        <div
+          class="current-pet-row"
+          v-for="{
+            id,
+            nickname,
+            imageUrl,
+            species,
+            registeredAt
+          } in currentPets"
+          :key="id"
+        >
+          <b-img thumbnail rounded="circle" class="pet-image" :src="imageUrl" />
+          <div class="info">
+            <div>{{ nickname }}</div>
+            <species-badge :species="species" />
+          </div>
+          <div class="register-date">Since {{ registeredAt }}</div>
+        </div>
+      </div>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from 'vue';
+import { Pet } from '@/types';
+import SpeciesBadge from '@/components/SpeciesBadge.vue';
+
+type PetWithRegisteredAt = Pet & { registeredAt: string };
+
+export default Vue.extend({
+  name: 'user-current-pets',
+  components: {
+    SpeciesBadge
+  },
+  props: {
+    currentPets: {
+      type: Array as PropType<PetWithRegisteredAt[]>,
+      default: []
+    }
+  }
+});
+</script>
+
+<style scoped>
+div.card {
+  box-shadow: 1px 3px 8px -3px gray;
+}
+
+div.current-pet-row {
+  display: grid;
+  grid-template-columns: 1fr 3fr 2fr;
+  align-items: center;
+  column-gap: 5px;
+
+  & > .info {
+    text-transform: capitalize;
+    font-size: 1.1rem;
+    font-weight: bold;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+
+  & > .pet-image {
+    object-fit: cover;
+    height: 50px;
+    width: 50px;
+  }
+
+  & > .register-date {
+    font-size: 0.9rem;
+    text-align: center;
+  }
+}
+</style>
