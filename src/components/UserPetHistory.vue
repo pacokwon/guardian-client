4fb35380162bc695fa11c0b87bdee091e318abc3
pacokwon <haechank@gmail.com>
2021-01-28 18:55:50 +0900

    wip: UserPetHistory and UserDetail implementation (almost identical to Pet)

---
+++ src/components/UserPetHistory.vue
@@ -0,0 +1,246 @@
+<template>
+  <b-card no-body>
+    <b-card-body>
+      <h4 class="card-title">
+        Pet History of
+        <span style="text-transform: capitalize;">{{ userNickname }}</span>
+      </h4>
+      <section>
+        <div class="history-row-header">
+          <b-button-group>
+            <b-button
+              class="pagination-button"
+              :disabled="page === 1 && !hasNextPage"
+              @click="fetchNextPage"
+            >
+              <b-icon-chevron-right />
+            </b-button>
+          </b-button-group>
+          <div>Guardian</div>
+          <div>Duration</div>
+        </div>
+        <div
+          class="history-row"
+          v-for="{ id, pet, registeredAt, releasedAt, released } in history"
+          :key="id"
+        >
+          <div class="avatar">
+            <b-img thumbnail rounded="circle" :src="pet.imageUrl" />
+          </div>
+          <div class="nickname">
+            {{ pet.nickname }}
+          </div>
+          <div class="duration" v-if="released">
+            {{ registeredAt.toISOString().slice(0, 10) }} ~
+            {{ releasedAt.toISOString().slice(0, 10) }}
+          </div>
+          <div class="duration" v-else>
+            {{ registeredAt.toISOString().slice(0, 10) }} ~
+            <strong style="color: var(--guardian-primary)">Current</strong>
+          </div>
+        </div>
+      </section>
+    </b-card-body>
+  </b-card>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import gql from 'graphql-tag';
+import { UserPetHistory, Node } from '@/types';
+
+type UserPetHistoryInput = Omit<
+  UserPetHistory,
+  'releasedAt' | 'registeredAt'
+> & {
+  releasedAt: string; // ISO String
+  registeredAt: string; // ISO String
+};
+
+const fetchHistoryQuery = gql`
+  query($userID: ID!, $first: Int!, $cursor: String) {
+    user(id: $userID) {
+      id
+      petHistory(first: $first, after: $cursor) {
+        pageInfo {
+          hasNextPage
+          endCursor
+        }
+        edges {
+          node {
+            id
+            pet {
+              id
+              nickname
+              species
+              imageUrl
+            }
+            releasedAt
+            registeredAt
+            released
+          }
+        }
+      }
+    }
+  }
+`;
+
+export default Vue.extend({
+  name: 'user-pet-history',
+  props: {
+    userID: {
+      type: String,
+      required: true
+    },
+    userNickname: {
+      type: String,
+      required: true
+    }
+  },
+  data() {
+    return {
+      page: 1,
+      pageSize: 4,
+      cursor: null as null | string,
+      hasNextPage: true,
+      history: [] as UserPetHistory[]
+    };
+  },
+  async mounted() {
+    const { userID } = this;
+    const result = await this.$apollo.query({
+      query: fetchHistoryQuery,
+      variables: { userID, first: this.pageSize }
+    });
+
+    const { petHistory = [] } = result?.data?.user || {};
+    this.history = (petHistory?.edges || []).map(
+      ({ node }: Node<UserPetHistoryInput>) => {
+        const { releasedAt, registeredAt, ...rest } = node;
+        return {
+          releasedAt: new Date(releasedAt),
+          registeredAt: new Date(registeredAt),
+          ...rest
+        };
+      }
+    );
+
+    const { hasNextPage, endCursor } =
+      result?.data?.user?.petHistory?.pageInfo || {};
+    this.hasNextPage = hasNextPage;
+    this.cursor = endCursor;
+
+    console.log({ hasNextPage });
+  },
+  methods: {
+    async fetchNextPage() {
+      this.page = this.hasNextPage ? this.page + 1 : 1;
+
+      const { userID } = this;
+      const result = await this.$apollo.query({
+        query: fetchHistoryQuery,
+        variables: {
+          userID,
+          first: this.pageSize,
+          cursor: this.hasNextPage ? this.cursor : undefined
+        }
+      });
+
+      const { petHistory = [] } = result?.data?.user || {};
+      this.history = (petHistory?.edges || []).map(
+        ({ node }: Node<UserPetHistoryInput>) => {
+          const { releasedAt, registeredAt, ...rest } = node;
+          return {
+            releasedAt: new Date(releasedAt),
+            registeredAt: new Date(registeredAt),
+            ...rest
+          };
+        }
+      );
+
+      const { hasNextPage, endCursor } =
+        result?.data?.user?.petHistory?.pageInfo || {};
+      this.hasNextPage = hasNextPage;
+      this.cursor = endCursor;
+    }
+  }
+});
+</script>
+
+<style scoped>
+div.card {
+  box-shadow: 1px 3px 8px -3px gray;
+}
+
+h4.card-title {
+  margin-bottom: 32px;
+}
+
+div.history-row,
+div.history-row-header {
+  font-size: 1.1rem;
+  display: grid;
+  grid-template-columns: 1fr 2fr 2fr;
+  max-width: 600px;
+  margin-bottom: 16px;
+  align-items: center;
+  justify-items: center;
+
+  & .avatar {
+    & > img {
+      height: 50px;
+      width: 50px;
+    }
+  }
+
+  & .nickname {
+    text-transform: capitalize;
+  }
+
+  & .duration {
+    font-size: 0.9rem;
+  }
+}
+
+div.history-row-header {
+  font-weight: bold;
+}
+
+div.history-row {
+  transition: transform 0.2s ease;
+
+  &:hover {
+    background-color: #f9f9f9;
+    transform: translate(0, -3px);
+  }
+}
+
+button.pagination-button {
+  /* override bootstrap styles for reduced button size */
+  --point-color: #8a90eb;
+  --active-color: #767de8;
+  --border-color: #a7abeb;
+
+  padding: 0.2rem 0.5rem;
+  font-size: 0.6rem;
+  color: var(--point-color);
+  border-color: var(--border-color);
+  background-color: white;
+
+  &:hover:enabled {
+    background-color: var(--point-color);
+    color: white;
+  }
+
+  &:active:enabled {
+    background-color: var(--active-color) !important;
+    border-color: var(--border-color) !important;
+    color: white;
+  }
+
+  &[disabled],
+  &:disabled {
+    cursor: not-allowed;
+  }
+}
+</style>
---
+++ src/pages/UserDetail.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="user-detail">
+    <transition name="fade-move" appear>
+      <user-profile
+        class="user-detail--profile"
+        :id="id"
+        :nickname="nickname"
+      />
+    </transition>
+    <transition name="fade-move" appear>
+      <user-current-pets
+        class="user-detail--pets"
+        :current-pets="currentPets"
+      />
+    </transition>
+    <div class="user-detail--history">
+      bar
+    </div>
+    <b-spinner v-if="userInfoLoading" variant="primary" type="grow" />
+    <transition v-else name="fade-move" appear>
+      <user-pet-history
+        class="user-detail--history"
+        :userID="id"
+        :userNickname="nickname"
+      />
+    </transition>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from 'vue';
+import gql from 'graphql-tag';
+import { Pet } from '@/types';
+import UserProfile from '@/components/UserProfile.vue';
+import UserCurrentPets from '@/components/UserCurrentPets.vue';
+import UserPetHistory from '@/components/UserPetHistory.vue';
+
+type PetWithRegisteredAt = Pet & { registeredAt: string };
+
+export default Vue.extend({
+  name: 'user-detail',
+  components: {
+    UserProfile,
+    UserCurrentPets,
+    UserPetHistory
+  },
+  data() {
+    return {
+      id: '',
+      nickname: '',
+      currentPets: [] as PetWithRegisteredAt[],
+      userInfoLoading: true
+    };
+  },
+  async mounted() {
+    const { userID } = this.$route.params;
+
+    const result = await this.$apollo.query({
+      query: gql`
+        query($userID: ID!) {
+          user(id: $userID) {
+            id
+            nickname
+            currentPets {
+              pet {
+                id
+                nickname
+                species
+                imageUrl
+              }
+              registeredAt
+            }
+          }
+        }
+      `,
+      variables: { userID }
+    });
+
+    const { id, nickname, currentPets } = result?.data?.user || {};
+    this.id = id;
+    this.nickname = nickname;
+    this.currentPets = currentPets.map(
+      ({ pet, registeredAt }: { pet: Pet; registeredAt: string }) => ({
+        ...pet,
+        registeredAt: registeredAt.slice(0, 10)
+      })
+    );
+
+    this.userInfoLoading = false;
+  }
+});
+</script>
+
+<style scoped lang="scss">
+div.user-detail {
+  display: grid;
+  gap: 16px 16px;
+  grid-template-columns: repeat(5, 1fr);
+  grid-template-areas:
+    'profile  profile  history history history'
+    'profile  profile  history history history'
+    'profile  profile  history history history'
+    'pets     pets     history history history'
+    'pets     pets     history history history';
+
+  &--profile {
+    grid-area: profile;
+  }
+
+  &--pets {
+    grid-area: pets;
+  }
+
+  &--history {
+    grid-area: history;
+  }
+
+  @media (max-width: 768px) {
+    grid-template-columns: 1fr;
+    grid-template-areas:
+      'profile'
+      'guardian'
+      'history';
+  }
+}
+</style>
