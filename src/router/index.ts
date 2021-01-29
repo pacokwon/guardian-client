7a534319776e5b0b931487a80bc13bd0b1688986
pacokwon <haechank@gmail.com>
2021-01-29 13:33:26 +0900

    rename: Home -> GuardianHome and remove HelloWorld

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,14 +1,16 @@
 import Vue from 'vue';
 import VueRouter, { RouteConfig } from 'vue-router';
-import Home from '../pages/Home.vue';
 
 Vue.use(VueRouter);
 
 const routes: Array<RouteConfig> = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    name: 'GuardianHome',
+    component: () =>
+      import(
+        /* webpackChunkName: "guardian-home" */ '../pages/GuardianHome.vue'
+      )
   },
   {
     path: '/pets',
