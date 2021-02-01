5c9e0d2fbdd316adc47b508b92cfbdd66e57d269
pacokwon <haechank@gmail.com>
2021-02-01 17:44:49 +0900

    update: loosen apollo client cache policy

--- src/graphql/apolloProvider.ts
+++ src/graphql/apolloProvider.ts
@@ -1,11 +1,18 @@
-import { ApolloClient } from 'apollo-client';
+import { ApolloClient, DefaultOptions } from 'apollo-client';
 import { createHttpLink } from 'apollo-link-http';
 import { InMemoryCache } from 'apollo-cache-inmemory';
 import VueApollo from 'vue-apollo';
 
+const defaultOptions: DefaultOptions = {
+  query: {
+    fetchPolicy: 'network-only'
+  }
+};
+
 const apolloClient = new ApolloClient({
   link: createHttpLink({ uri: 'http://localhost:3000/graphql' }),
-  cache: new InMemoryCache()
+  cache: new InMemoryCache(),
+  defaultOptions
 });
 
 export const apolloProvider = new VueApollo({ defaultClient: apolloClient });
